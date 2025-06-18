55fde8ccc6b1518ca61f80bb874ba76d8bee945d
Ranaa-Mohammed <rm2174819@gmail.com>
2025-06-19 01:45:49 +0300

    add fontSize

--- lib/components/newText.dart
+++ lib/components/newText.dart
@@ -8,7 +8,7 @@ class NewText extends StatelessWidget {
     return const Scaffold(
       body: Column(
         children: [
-          Text('Helllllo ', style: TextStyle(color: Colors.teal),),
+          Text('Helllllo ', style: TextStyle(color: Colors.teal, fontSize: 30),),
         ],
       ),
     );
